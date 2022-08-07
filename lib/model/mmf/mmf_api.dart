7b99f3bc231b41777d4a189b44ecb0b8e67405a1
Trevor Claridge <trevclaridge@gmail.com>
2022-08-07 00:39:18 -0400

    add object schema, edit user schema

--- lib/model/mmf/mmf_api.dart
+++ lib/model/mmf/mmf_api.dart
@@ -8,11 +8,26 @@ class MMFAPI {
   }
   MMFAPI._internal();
 
-  String _baseUrl = 'https://www.myminifactory.com/api/v2/';
-  String _apiKey = '36be06aa-5d9d-4346-bfeb-7e9cd8eb9f9b';
-  String _clientId = '3DPrintingProfile';
+  final String _baseUrl = 'https://www.myminifactory.com/api/v2/';
+  final String _apiKey = '36be06aa-5d9d-4346-bfeb-7e9cd8eb9f9b';
+  final String _clientId = '3DPrintingProfile';
 
   Future<MMFUser> fetchMMFUser(String user) async {
+    final response = await http.get(
+      Uri.parse(
+          'https://www.myminifactory.com/api/v2/users/$user?key=$_apiKey'),
+    );
+
+    if (response.statusCode == 200) {
+      print(response.body);
+      return MMFUser.fromJson(jsonDecode(response.body));
+    } else {
+      print(response.body);
+      throw Exception('Failed to load album');
+    }
+  }
+
+  Future<MMFUser> fetchUserLikes(String user) async {
     final response = await http.get(
       Uri.parse(
           'https://www.myminifactory.com/api/v2/users/$user?key=$_apiKey'),
--- lib/model/model.dart
+++ lib/model/model.dart
@@ -2,11 +2,12 @@ library model;
 
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:url_launcher/url_launcher.dart';
 
-part 'mmf/mmf_api.dart';
-part 'mmf/mmf_user.dart';
-part 'mmf/mmf_auth.dart';
 part 'auth/auth_token.dart';
 part 'auth/startup.dart';
+part 'mmf/mmf_api.dart';
+part 'mmf/mmf_auth.dart';
+part 'mmf/schema/mmf_object.dart';
+part 'mmf/schema/mmf_user.dart';
